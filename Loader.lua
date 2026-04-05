411dcffeed0123b03ee708d678fa0b74a969b2fd
Marko <marko@github.com>
2026-04-05 21:52:40 +0200

    Added Ruby Hub Loader

---
+++ Loader.lua
@@ -0,0 +1,30 @@
+--[[
+    ___       __          __ __     __ 
+   / _ \__ __/ /  __ __  / // /_ __/ / 
+  / , _/ // / _ \/ // / / _  / // / _ \
+ /_/|_|\_,_/_.__/\_, / /_//_/\_,_/_.__/
+                /___/                  
+
+    Loader  |  2026-04-05  |  Roblox Script Hub
+    
+    
+    Author: Ruby Hub
+    Github: https://github.com/aymarko/RubyHub
+    Website: https://rubyhub.net
+    Discord: https://discord.gg/rubyhub
+]]
+
+local gameId = game.PlaceId
+
+if gameId == 1224212277 then
+    loadstring(game:HttpGet('https://raw.githubusercontent.com/aymarko/RubyHub/main/MadCity/Chapter2/Main.lua'))()
+    
+elseif gameId == 91282350711571 then
+    _G.AutorobIn = "public"
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/aymarko/RubyHub/main/MadCity/Chapter1/Autorob.lua"))()
+
+else
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/aymarko/RubyHub/main/MadCity/Chapter2/Modules/Notify.lua", true))()
+    task.wait(0.5)
+    _G.CustomNotify({{Text = "his game is not supported by Ruby Hub!", Delay = 5}})
+end
